9ed3c6c9a34bf874ee8b9ff232922b0a9b92aa34
vvjo <vaino.kojo@metropolia.fi>
2019-12-08 19:43:28 +0200

    catch korjaus

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ac70cd8623dbbfa2b5c8009a7ea4480.js"
+  "/precache-manifest.3e36d6bd8a168c607f8129ad2728c0fb.js"
 );
 
 self.addEventListener('message', (event) => {
